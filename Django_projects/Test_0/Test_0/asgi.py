292afbcb7d1fd328b84400feb56b22965d283a4f
JRBhat <jayesh.r.bhat@gmail.com>
2024-02-28 20:19:52 +0100

    django test directory restructured

--- Django_projects/Test_0/Test_0/asgi.py
+++ Django_projects/Test_0/Test_0/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Test1 project.
+ASGI config for Test_0 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Test1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Test_0.settings')
 
 application = get_asgi_application()
--- Django_projects/Test_0/Test_0/urls.py
+++ Django_projects/Test_0/Test_0/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Test1 project.
+URL configuration for Test_0 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
